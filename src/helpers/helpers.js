f76c0bd4820501bf4464dc3d14a61b61745ea7f3
VigM <vmikulis@figure.com>
2021-12-20 11:20:01 -0700

    version up.  export sha256 helper.  update sign jwt format and value

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -2,7 +2,7 @@ import { createHash } from 'crypto';
 import { fromBase58 } from 'bip32';
 import { ecdsaVerify } from 'secp256k1';
 
-const sha256 = (message) => createHash('sha256').update(Buffer.from(message,"utf-8")).digest();
+export const sha256 = (message) => createHash('sha256').update(Buffer.from(message,"utf-8")).digest();
 
 export const verifySignature = async (
   message, signature, publicKey, network,
--- src/services/methods/signJWT.js
+++ src/services/methods/signJWT.js
@@ -1,7 +1,7 @@
 import base64url from 'base64url';
 import { convertUtf8ToHex } from '@walletconnect/utils';
 import { GET_PROVENANCE_NETWORK } from '../../consts';
-import { verifySignature } from '../../helpers';
+import { verifySignature, sha256 } from '../../helpers';
 
 export const signJWT = async (state, networkName) => {
   const { connector, address, publicKey } = state;
@@ -10,13 +10,20 @@ export const signJWT = async (state, networkName) => {
 
   if (!connector) return { method, error: 'No wallet connected' };
   // Build JWT
-  const expires = Math.floor(Date.now() / 1000) + 900; // 900s (15min)
-  const header = JSON.stringify({alg: 'ES256', typ: 'JWT'});
+  const now = Math.floor(Date.now() / 1000); // Current time
+  const expires = now + 900; // 900s (15min)
+  const header = JSON.stringify({alg: 'ES256K', typ: 'JWT'});
   const headerEncoded = base64url(header);
   const publicKeyEncoded = base64url(publicKey);
-  const payload = JSON.stringify({sub: `${publicKeyEncoded},${address}`, iss: 'provenance.io', iat: expires, exp: expires});
+  const payload = JSON.stringify({
+    sub: publicKeyEncoded,
+    iss: 'provenance.io',
+    iat: now,
+    exp: expires,
+    addr: address,
+  });
   const payloadEncoded = base64url(payload);
-  const jwtEncoded = base64url(`${header}.${payload}`);
+  const jwtEncoded256 = sha256(`${headerEncoded}.${payloadEncoded}`);
   // prov_sign params
   const metadata = JSON.stringify({
     description: 'Sign JWT Token',
@@ -26,8 +33,8 @@ export const signJWT = async (state, networkName) => {
       even_more: 'stuff',
     },
   });
-  const hexJWT = convertUtf8ToHex(jwtEncoded);
-  const msgParams = [metadata, hexJWT];
+  const hexJWT256 = convertUtf8ToHex(jwtEncoded256);
+  const msgParams = [metadata, hexJWT256];
   // Custom Request
   const customRequest = {
     id: 1,
@@ -41,7 +48,7 @@ export const signJWT = async (state, networkName) => {
     // result is a hex encoded signature
     const signature = Uint8Array.from(Buffer.from(result, 'hex'));
     // verify signature
-    const valid = await verifySignature(jwtEncoded, signature, publicKey, network);
+    const valid = await verifySignature(jwtEncoded256, signature, publicKey, network);
     // const { signedPayload } = result?.message;
     // const signedPayloadEncoded = base64url(signedPayload);
     // const signedJWT = `${headerEncoded}.${payloadEncoded}.${signedPayloadEncoded}`;
