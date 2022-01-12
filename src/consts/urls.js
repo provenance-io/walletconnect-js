927fe7bbc9c1b0109785f35e33c7745073f06944
VigM <vmikulis@figure.com>
2022-01-12 09:56:47 -0700

    version up.  Update bridge url to figure

--- src/consts/urls.js
+++ src/consts/urls.js
@@ -1,4 +1,3 @@
-export const WALLETCONNECT_BRIDGE_URL = 'https://bridge.walletconnect.org';
-// export const WALLETCONNECT_BRIDGE_URL = 'wss://test.figure.tech/service-wallet-connect-bridge/ws/external';
+export const WALLETCONNECT_BRIDGE_URL = 'wss://test.figure.tech/service-wallet-connect-bridge/ws/external';
 export const PLUGIN_FIGURE_WALLET = 'figurewallet://';
 export const PLUGIN_PROVENANCE_WALLET = 'provenancewallet://';
