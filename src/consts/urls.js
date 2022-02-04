6a0c5064a0168a4d581b71f0143a40b056be157c
VigM <vmikulis@figure.com>
2022-02-04 12:47:50 -0700

    update url

--- src/consts/urls.js
+++ src/consts/urls.js
@@ -2,4 +2,4 @@ export const WALLETCONNECT_BRIDGE_URL = 'wss://test.figure.tech/service-wallet-c
 // export const WALLETCONNECT_BRIDGE_URL = 'https://bridge.walletconnect.org';
 export const PLUGIN_FIGURE_WALLET = 'figurewallet://';
 export const PLUGIN_PROVENANCE_WALLET = 'provenancewallet://';
-export const UNICORN_WALLET_URL = 'sparklyunicorn://wc:de773163-e902-45ec-ba20-fa6c8c98fa8a@1?key=8edadb8b525e67db72e660763196427f7587ab5f9c9de3903f855f0dd4fc612f';
+export const UNICORN_WALLET_URL = 'sparklyunicorn://';
