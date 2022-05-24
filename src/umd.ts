ce3c08f7a916bcd5d90480a04a3d4dd1a7a06304
VigM <vmikulis@figure.com>
2022-05-24 13:15:46 -0600

    remove extra comment

--- src/umd.ts
+++ src/umd.ts
@@ -1,8 +1,6 @@
 import { WalletConnectService } from './services';
 import { WINDOW_MESSAGES } from './consts/windowMessages';
-  
-// NOTE: When we include WalletConnectService the standalone lib fails
-// When we only export the test and WINDOW_MESSAGES it works as expected.
+
 export {
   WINDOW_MESSAGES,
   WalletConnectService,
