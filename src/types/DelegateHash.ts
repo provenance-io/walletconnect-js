256d8c594fd57aa2ee23d0ed5f1e83ae963c5052
VigM <vmikulis@figure.com>
2022-10-12 13:46:17 -0600

    changes towards 2.0.0

---
+++ src/types/DelegateHash.ts
@@ -0,0 +1,11 @@
+import type { GasPrice } from './GasPriceType';
+
+interface Amount {
+  amount: number;
+  gasPrice?: GasPrice;
+}
+
+export interface DelegateHashData extends Amount {
+  validatorAddress: string;
+  to: never;
+}
---
+++ src/types/WalletConnectService.ts
@@ -0,0 +1,43 @@
+import type WalletConnectClient from '@walletconnect/client';
+import type { IClientMeta } from './IClientMeta';
+import type { WalletId } from './WalletList';
+import type { WalletInfo } from './ConnectData';
+
+export type WalletConnectClientType = WalletConnectClient;
+
+export interface WCSState {
+  account: string;
+  address: string;
+  bridge: string;
+  connected: boolean;
+  connectionEat: number | null;
+  connectionIat: number | null;
+  connectionTimeout: number;
+  connector: WalletConnectClientType | null;
+  figureConnected: boolean;
+  isMobile: boolean;
+  loading: string;
+  newAccount: boolean;
+  peer: IClientMeta | null;
+  publicKey: string;
+  QRCode: string;
+  QRCodeUrl: string;
+  showQRCodeModal: boolean;
+  signedJWT: string;
+  walletApp?: WalletId | '';
+  walletInfo: WalletInfo;
+}
+
+export type WCSSetState = (state: Partial<WCSState>) => void;
+export type WCSSetFullState = (state: WCSState) => void;
+
+export interface WCJSLocalState {
+  account: string;
+  connectionEat: number;
+  connectionIat: number;
+  connectionTimeout: number;
+  figureConnected: boolean;
+  newAccount: false;
+  signedJWT: string;
+  walletApp?: WalletId | '';
+}
