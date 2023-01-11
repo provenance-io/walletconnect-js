21da5fffcba5013e88df8629e2f831f592405163
VigM <vmikulis@figure.com>
2023-01-11 14:34:14 -0700

    buildDemo will correctly build out the walletconnect demo repo for us now

--- scripts/buildDemo.js
+++ scripts/buildDemo.js
@@ -1,15 +1,17 @@
 // Clone the react example to build out the walletconnect demo app for deployment
 module.exports.buildDemo = function () {
   // IMPORTS
-  const { promises: fs } = require('fs');
+  const fs = require('fs');
   const path = require('path');
   // CONSTANTS
   const exampleDirectory = 'examples/example-react';
   const demoFileName = 'figure-tech-walletconnect';
+  const packageJsonFile = require(`../${exampleDirectory}/package.json`);
+  const version = packageJsonFile.version;
   // FUNCTIONS
   async function copyDir(src, dest) {
-    await fs.mkdir(dest, { recursive: true });
-    let entries = await fs.readdir(src, { withFileTypes: true });
+    await fs.promises.mkdir(dest, { recursive: true });
+    let entries = await fs.promises.readdir(src, { withFileTypes: true });
 
     for (let entry of entries) {
       let srcPath = path.join(src, entry.name);
@@ -19,12 +21,47 @@ module.exports.buildDemo = function () {
 
         entry.isDirectory()
           ? await copyDir(srcPath, destPath)
-          : await fs.copyFile(srcPath, destPath);
+          : await fs.promises.copyFile(srcPath, destPath);
       }
     }
   }
+
+  // Change package.json to use latest version value (both example and wcjs use same version)
+  const updatePackageJson = async () => {
+    const regularExpressionStartScript = /"start":(.+),/i;
+    const regularExpressionWcjsPackage = /"@provenanceio\/walletconnect-js":(.+),/i;
+    const packageLocations = ['package.json', 'package-lock.json'];
+
+    // Loop through each package location to update the wcjs import
+    packageLocations.forEach((packageLocation) => {
+      const packageFileData = fs.readFileSync(
+        `${exampleDirectory}/${packageLocation}`,
+        'utf-8'
+      );
+      if (packageFileData) {
+        let updatedPackageData = '';
+        // find the package import and update the version
+        updatedPackageData = packageFileData.replace(
+          regularExpressionWcjsPackage,
+          `"@provenanceio/walletconnect-js": "${version}",`
+        );
+        if (packageLocation.includes('package.json')) {
+          // find the start script and update it
+          updatedPackageData = updatedPackageData.replace(
+            regularExpressionStartScript,
+            `"start": "react-scripts start",`
+          );
+        }
+        fs.writeFileSync(`${demoFileName}/${packageLocation}`, updatedPackageData);
+      }
+    });
+
+    return;
+  };
+
   const cloneExampleApp = async () => {
     await copyDir(exampleDirectory, demoFileName);
+    await updatePackageJson();
     return;
   };
   // INIT
