478d280296344642b182e43ea0ff45f781d1a970
VigM <vmikulis@figure.com>
2023-01-11 13:13:11 -0700

    update scripts to be js.  Add new script to build demo app from the example app

---
+++ scripts/buildDemo.js
@@ -0,0 +1,32 @@
+// Clone the react example to build out the walletconnect demo app for deployment
+module.exports.buildDemo = function () {
+  // IMPORTS
+  const { promises: fs } = require('fs');
+  const path = require('path');
+  // CONSTANTS
+  const exampleDirectory = 'examples/example-react';
+  const demoFileName = 'figure-tech-walletconnect';
+  // FUNCTIONS
+  async function copyDir(src, dest) {
+    await fs.mkdir(dest, { recursive: true });
+    let entries = await fs.readdir(src, { withFileTypes: true });
+
+    for (let entry of entries) {
+      let srcPath = path.join(src, entry.name);
+      // Don't copy any node_modules
+      if (!srcPath.includes('node_modules')) {
+        let destPath = path.join(dest, entry.name);
+
+        entry.isDirectory()
+          ? await copyDir(srcPath, destPath)
+          : await fs.copyFile(srcPath, destPath);
+      }
+    }
+  }
+  const cloneExampleApp = async () => {
+    await copyDir(exampleDirectory, demoFileName);
+    return;
+  };
+  // INIT
+  cloneExampleApp();
+};
---
+++ scripts/commentSniffer.js
@@ -0,0 +1,96 @@
+"use strict";
+exports.__esModule = true;
+module.exports.commentSniffer = function () {
+    var glob = require('glob'); // eslint-disable-line @typescript-eslint/no-var-requires
+    var fs = require('fs'); // eslint-disable-line @typescript-eslint/no-var-requires
+    var PrettyError = require('pretty-error'); // eslint-disable-line @typescript-eslint/no-var-requires
+    var getDirectories = function (src, callback) {
+        var res = glob.sync(src + '/**/*');
+        callback(res);
+    };
+    var fileTypes = ['.ts', '.tsx', '.js', '.jsx', '.md', '.mdx'];
+    var comments = [
+        'TEMP:',
+        'TEMPONLY:',
+        'TESTING:',
+        'TEST:',
+        'TESTONLY:',
+        'TESTINGONLY:',
+        'REMOVEME:',
+        'REMOVE:',
+        'HACK:',
+    ];
+    var rootDirs = ['src', 'webDemo/src'];
+    var checkIfValidFile = function (fileSrc) {
+        var valid = false;
+        fileTypes.forEach(function (fileType) {
+            if (fileSrc.includes(fileType)) {
+                valid = true;
+            }
+        });
+        return valid;
+    };
+    var checkIfCommentMatches = function (fileData) {
+        var commentExists = false;
+        comments.forEach(function (comment) {
+            if (fileData.includes(comment)) {
+                commentExists = true;
+            }
+        });
+        return commentExists;
+    };
+    var runSniffer = function () {
+        var allErrors = [];
+        rootDirs.forEach(function (rootDir) {
+            getDirectories(rootDir, function (res) {
+                // Look at each file
+                res.forEach(function (fileLocation) {
+                    var valid = checkIfValidFile(fileLocation);
+                    if (valid) {
+                        var data = fs.readFileSync(fileLocation);
+                        var hasComment = checkIfCommentMatches(data);
+                        if (hasComment) {
+                            allErrors.push(fileLocation);
+                        }
+                    }
+                });
+            });
+        });
+        if (allErrors.length) {
+            var message_1 = 'Dev Local Only Comments Found!  Please Fix/Remove:\n --------------------------------------------------\n';
+            allErrors.forEach(function (msg) {
+                message_1 += " \n \u2022 ".concat(msg);
+            });
+            var pe = new PrettyError();
+            pe.appendStyle({
+                'pretty-error > header > title > kind': { display: 'none' },
+                'pretty-error > header > colon': { display: 'none' },
+                'pretty-error > header > message': {
+                    color: 'bright-white',
+                    background: 'red',
+                    padding: '0 1'
+                },
+                'pretty-error > trace > item': { display: 'none' }
+            });
+            var renderedError = pe.render(new Error(message_1));
+            console.log(renderedError); // eslint-disable-line no-console
+            process.exit(1);
+        }
+        else {
+            var pe = new PrettyError();
+            pe.appendStyle({
+                'pretty-error > header > title > kind': { display: 'none' },
+                'pretty-error > header > colon': { display: 'none' },
+                'pretty-error > header > message': {
+                    color: 'bright-white',
+                    background: 'green',
+                    padding: '0 1'
+                },
+                'pretty-error > trace > item': { display: 'none' }
+            });
+            var renderedSuccess = pe.render(new Error('All comment checks passed'));
+            console.log(renderedSuccess); // eslint-disable-line no-console
+        }
+    };
+    runSniffer();
+};
