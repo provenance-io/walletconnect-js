2a919564286ee4b53e3c0e738736c09b0a1938d4
VigM <vmikulis@figure.com>
2023-02-15 14:22:42 -0700

    update to match mobile dynamic link example in test

--- src/__tests__/unit/generateDynamicUrl.test.ts
+++ src/__tests__/unit/generateDynamicUrl.test.ts
@@ -2,9 +2,7 @@ import {
   DYNAMIC_LINK_FIGURE_MOBILE_URL,
   FIGURE_MOBILE_WALLET_CONNECT_URL,
   FIGURE_MOBILE_WALLET_APP_ID,
-  FIGURE_MOBILE_WALLET_APP_ID_TEST,
   FIGURE_MOBILE_WALLET_PACKAGE_NAME,
-  FIGURE_MOBILE_WALLET_PACKAGE_NAME_TEST,
 } from '../../consts';
 import { generateDynamicUrl } from '../../utils';
 
@@ -16,22 +14,10 @@ const urlDataArray = [
     originUrl: DYNAMIC_LINK_FIGURE_MOBILE_URL,
     packageName: FIGURE_MOBILE_WALLET_PACKAGE_NAME,
     qRCodeUrl:
-      'wc%3A94028ac0-eef7-4a87-a910-065cf3b75bfe%401%3Fbridge%3Dwss%253A%252F%252Fwww.figure.tech%252Fservice-wallet-connect-bridge%252Fws%252Fexternal%26key%3D46efe12f61e37964ab05782ce373b736e1ece635f1da6f55d619d1a5876bc256',
+      'wc%3A79d1d765-8d4c-49f6-95d8-baffeeb22808%401%3Fbridge%3Dwss%253A%252F%252Fwww.figure.tech%252Fservice-wallet-connect-bridge%252Fws%252Fexternal%26key%3Da3a477709e4ad8bbd3bf1318eaf8c1930124b3afc8824859caa0612c2ca36f4f',
     walletConnectUrl: FIGURE_MOBILE_WALLET_CONNECT_URL,
     result:
-      'https://figurewallet.page.link?link=https%253A%252F%252Ffigure.com%252Fwallet-connect%253Fdata%253Dwc%25253A94028ac0-eef7-4a87-a910-065cf3b75bfe%2525401%25253Fbridge%25253Dwss%2525253A%2525252F%2525252Fwww.figure.tech%2525252Fservice-wallet-connect-bridge%2525252Fws%2525252Fexternal%252526key%25253D46efe12f61e37964ab05782ce373b736e1ece635f1da6f55d619d1a5876bc256&apn=com.figure.mobile.wallet&ibi=com.figure.mobile.wallet&isi=6444263900&efr=1',
-  },
-  // TEST URL
-  {
-    dataType: 'Test Url',
-    appId: FIGURE_MOBILE_WALLET_APP_ID_TEST,
-    originUrl: DYNAMIC_LINK_FIGURE_MOBILE_URL,
-    packageName: FIGURE_MOBILE_WALLET_PACKAGE_NAME_TEST,
-    qRCodeUrl:
-      'wc%3A94028ac0-eef7-4a87-a910-065cf3b75bfe%401%3Fbridge%3Dwss%253A%252F%252Fwww.figure.tech%252Fservice-wallet-connect-bridge%252Fws%252Fexternal%26key%3D46efe12f61e37964ab05782ce373b736e1ece635f1da6f55d619d1a5876bc256',
-    walletConnectUrl: FIGURE_MOBILE_WALLET_CONNECT_URL,
-    result:
-      'https://figurewallet.page.link?link=https%253A%252F%252Ffigure.com%252Fwallet-connect%253Fdata%253Dwc%25253A94028ac0-eef7-4a87-a910-065cf3b75bfe%2525401%25253Fbridge%25253Dwss%2525253A%2525252F%2525252Fwww.figure.tech%2525252Fservice-wallet-connect-bridge%2525252Fws%2525252Fexternal%252526key%25253D46efe12f61e37964ab05782ce373b736e1ece635f1da6f55d619d1a5876bc256&apn=com.figure.mobile.wallet.dev&ibi=com.figure.mobile.wallet.dev&isi=6444293331&efr=1',
+      'https://figurewallet.page.link?link=https%3A%2F%2Ffigure.com%2Fwallet-connect%3Fdata%3Dwc%25253A79d1d765-8d4c-49f6-95d8-baffeeb22808%2525401%25253Fbridge%25253Dwss%2525253A%2525252F%2525252Fwww.figure.tech%2525252Fservice-wallet-connect-bridge%2525252Fws%2525252Fexternal%252526key%25253Da3a477709e4ad8bbd3bf1318eaf8c1930124b3afc8824859caa0612c2ca36f4f&apn=com.figure.mobile.wallet&ibi=com.figure.mobile.wallet&isi=6444263900&efr=1',
   },
 ];
 
--- src/utils/generateDynamicUrl.ts
+++ src/utils/generateDynamicUrl.ts
@@ -1,4 +1,3 @@
-// Mobile wallets have a dynamic url for creating their full urls.  This url, when clicked on by a mobile device, will prompt the phones browser to open their existing wallet app or direct them to the app store to download it.  Moved this function into a util in order to write unit tests around it.
 interface GenerateDyanmicUrl {
   qRCodeUrl: string;
   originUrl: string;
@@ -8,6 +7,8 @@ interface GenerateDyanmicUrl {
   efr?: string;
 }
 
+// Mobile wallets have a dynamic url for creating their full urls.  This url, when clicked on by a mobile device, will prompt the phones browser to open their existing wallet app or direct them to the app store to download it.  Moved this function into a util in order to write unit tests around it.
+// Ref: https://www.notion.so/figuretech/Deep-Linking-25b15c3483f24d68b5ddb4cd2ff6f8fb
 export const generateDynamicUrl = ({
   qRCodeUrl,
   originUrl,
@@ -16,14 +17,13 @@ export const generateDynamicUrl = ({
   appId,
   efr = '1',
 }: GenerateDyanmicUrl) => {
-  const doubleEncode = (value: string) =>
-    encodeURIComponent(encodeURIComponent(value));
-  // Note: We need to double encode each url param (refer to mobile team)
-  const linkParam = doubleEncode(`${walletConnectUrl}?data=${qRCodeUrl}`);
-  const apnParam = doubleEncode(packageName);
-  const ibiParam = doubleEncode(packageName);
-  const isiParam = doubleEncode(appId);
-  const efrParam = doubleEncode(efr);
+  const linkParam = encodeURIComponent(
+    `${walletConnectUrl}?data=${encodeURIComponent(qRCodeUrl)}`
+  );
+  const apnParam = packageName;
+  const ibiParam = packageName;
+  const isiParam = appId;
+  const efrParam = efr;
   const fullUrlParamsEncoded = `?link=${linkParam}&apn=${apnParam}&ibi=${ibiParam}&isi=${isiParam}&efr=${efrParam}`;
   const fullUrl = `${originUrl}${fullUrlParamsEncoded}`;
 
