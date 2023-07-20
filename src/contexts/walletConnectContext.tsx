6878a3aefd474655f12cf271f0289cfe43319d6c
Jordan Farrer <jfarrer@figure.com>
2023-07-20 12:40:00 -0600

    feat: add hosted wallet iframe

--- src/contexts/walletConnectContext.tsx
+++ src/contexts/walletConnectContext.tsx
@@ -1,4 +1,5 @@
 import React, { createContext, useContext, useEffect, useState } from 'react';
+import { HostedWalletIframe } from '../Components/HostedWalletIframe';
 import { WalletConnectService } from '../services';
 import type { WCSState } from '../types';
 
@@ -67,6 +68,7 @@ const WalletConnectContextProvider: React.FC<Props> = ({
 
   return (
     <StateContext.Provider value={{ walletConnectService, walletConnectState }}>
+      <HostedWalletIframe />
       {children}
     </StateContext.Provider>
   );
