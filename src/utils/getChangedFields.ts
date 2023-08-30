9c6f865266fd2b5e04488b598529cb94e210f09b
VigM <vmikulis@figure.com>
2023-08-30 10:35:15 -0600

    commiting latest

--- src/utils/getChangedFields.ts
+++ src/utils/getChangedFields.ts
@@ -8,27 +8,24 @@ export const getChangedFields = (
   targetKeys: TargetKeyArray
 ) => {
   const changedValues: StorageObject = {};
-
-  // ISSUE: This function will only work for the first key of an object, eg: food.fruit
-  // It will not check food.fruit.apple
-  // - Option 1: We need to recursively check every field if it's an object (maybe a max depth? Wc obj must be deep...)
-  // -- Check each key until all "targetKeys" have been hit or there are no more objects to look over
-  // - Option 2: allow 'targetKeys' value to include a '.' indicating we want to find a nested key value
-  // -- Main usecase is wcs state which will now have all values nested once, eg: connection.connectionEST
-  // -- This would prevent having to loop over and over and we could target the exact spot in the object
-
-  // Recursive loop to check nested objects for their keys
-
   // We have a specific set of keys to look for differences with
   if (targetKeys) {
     targetKeys.forEach((key) => {
-      // targetKeys might be in "dot" notation, eg: wallet.address, check for them and look for those keys accordingly (nested)
+      // targetKeys will be in "dot" notation, eg: wallet.address, check for them and look for those keys accordingly (nested)
       // Note: We won't go deeper than 3 keys
-      const [key0, key1, key3] = key.split('.');
-      if (newObj[key0][key1][key3] !== oldObj[key0][key1][key3]) {
-        // Issue: How do we handle building the changedValues object with nesting?
-
-        if (key1 && !changedValues[key0][key1]) changedValues;
+      const [key0, key1, key2] = key.split('.');
+      const valueChanged = newObj[key0][key1][key2] !== oldObj[key0][key1][key2];
+      if (valueChanged) {
+        // Pre-populate objects as needed based on keys
+        if (key0 && !changedValues[key0]) changedValues[key0] = {};
+        if (key1 && !changedValues[key0][key1]) changedValues[key0][key1] = {};
+        if (key2 && !changedValues[key0][key1][key2])
+          changedValues[key0][key1][key2] = {};
+        // Add in the latest values
+        // Examples:
+        // wallet
+        // wallet.address
+        // wallet.data.name  // not a real one but needs to handle 3 keys in
         changedValues[key0] = newObj[key];
       }
     });
@@ -36,3 +33,25 @@ export const getChangedFields = (
 
   return changedValues;
 };
+
+/*
+  Where it's used:
+    - When we compare the current saved state for an object to a new saved state for an object.
+    - This will happen when one tab says "Switch active account". All the other tabs need to know that the currently selected/connected account has changed
+    - DApps need to be able to detect this change as well incase they don't support the selected account from one tab to the next
+  
+  How will it work:
+    - There will be an event listener looking for specific localStorage value changes
+    - We will be interested in specific values changes like:
+      - wallet.address, wallet.signedJWT, connection.walletAppId, connection.est, connection.ext, and connection.timeout
+      - We will also look through some walletconnect values too
+    - When we see these values changed, we will update our current state to match the new value
+      - Some of these values will potentially change other state values
+        - Eg: walletconnect.connected => connection.status
+      - The dApp will have it's own listeners setup to detect when these changes take place
+        - Eg: return wcs.state.connection.status === 'connected' ? 'Connected!' : 'Disconnected.'
+        
+  Concerns:
+    - Now that we don't broadcast anything, can non-react dApps catch these context state value changes correctly?
+      - They should since the state is shared in the context class, but needs testing in a non-react app
+*/
